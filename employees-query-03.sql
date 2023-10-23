c6001a5ecbe9b0d907d6a41aaa04cc0461774fa3
Shifa <luh.shifa@gmail.com>
2023-10-23 18:25:51 +0800

    temp

--- employees-query-03.sql
+++ employees-query-03.sql
@@ -4,9 +4,12 @@ select
     t.title as Position,
     t.from_date as "From",
     t.to_date as "To",
+#     if(t.to_date >= curdate(), 'now', t.to_date) as "To",
     if(e.gender='m', 'Male', 'Female') as Gender,
     e.hire_date as Hired
 from employees e
     inner join titles t on e.emp_no = t.emp_no
+
+where t.to_date = '9999/01/01'
 
 order by ID;
