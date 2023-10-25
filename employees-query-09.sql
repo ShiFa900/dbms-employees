543f9792727fac4bd6991b06f7845ebae12f4c88
Shifa <luh.shifa@gmail.com>
2023-10-25 13:40:43 +0800

    Wednesday, Oct 25 2023 (done yet)

--- employees-query-09.sql
+++ employees-query-09.sql
@@ -17,7 +17,6 @@ from employees e
 
          join(select tit.emp_no, MAX(tit.to_date) as max_title_to_date
               from titles tit
-              where tit.to_date in (select MAX(tit.to_date))
               group by tit.emp_no) as tit on tit.emp_no = e.emp_no
 
 
@@ -38,42 +37,47 @@ where (
 order by ID;
 
 
-SELECT e.emp_no                                 as ID,
-       concat(e.first_name, ' ', e.last_name)   as Name,
-       t.title                                  as Position,
-       t.from_date                              as "From",
-       t.to_date                                as "To",
-       if(e.gender = 'm', 'Male', 'Female')     as Gender,
-       e.hire_date                              as Hired,
-       d.dept_name                              as Department,
-       d.dept_no                                as "Department no",
-       de.from_date                             as "Assigned from",
-       de.to_date                               as "Assigned to",
+SELECT e.emp_no                               AS ID,
+       concat(e.first_name, ' ', e.last_name) AS Name,
+       t.title                                AS Position,
+       t.from_date                            AS "From",
+       t.to_date                              AS "To",
+       if(e.gender = 'm', 'Male', 'Female')   AS Gender,
+       e.hire_date                            AS Hired,
+       d.dept_name                            AS Department,
+       d.dept_no                              AS "Department no",
+       de.from_date                           AS "Assigned from",
+       de.to_date                             AS "Assigned to",
 #
-       concat(dm.first_name, ' ', em.last_name) as Manager,
-       s.salary                                 as Salary
+       concat(em.first_name, ' ', em.last_name) as Manager,
+       s.salary                               AS Salary
 FROM employees e
 
          JOIN (SELECT emp_no,
-                      max(titles.to_date) as titles_max_to_date
+                      max(titles.to_date) AS titles_max_to_date
                FROM titles
                GROUP BY emp_no) last_title ON last_title.emp_no = e.emp_no
          JOIN titles t ON t.emp_no = e.emp_no AND t.to_date = last_title.titles_max_to_date
 
-         JOIN dept_emp de on e.emp_no = de.emp_no
+         JOIN dept_emp de ON e.emp_no = de.emp_no
 
-         JOIN departments d on de.dept_no = d.dept_no
-         JOIN (SELECT emp_no,
-                      max(dept_manager.to_date) as dept_manager_latest
+         JOIN departments d ON de.dept_no = d.dept_no
+    #     Bagaimana menentukan employee A  dengan manager B?
+         JOIN (SELECT dept_no,
+                      max(dept_manager.to_date) as manager_max_to_date
                FROM dept_manager
-               GROUP BY emp_no) as latest_manager ON latest_manager.emp_no = e.emp_no
-         JOIN dept_manager dm on dm.emp_no = e.emp_no AND dm.to_date = latest_manager.dept_manager_latest
+               GROUP BY dept_no) last_manager ON last_manager.dept_no = d.dept_no
+         JOIN dept_manager dm
+              ON dm.dept_no = d.dept_no AND dm.to_date = last_manager.manager_max_to_date
+         JOIN employees em ON em.emp_no = dm.emp_no
 
          JOIN (SELECT emp_no,
-                      max(salaries.to_date) as salary_max_to_date
+                      max(salaries.to_date) AS sallary_max_to_date
                FROM salaries
-               GROUP BY emp_no) last_salary ON last_salary.emp_no = e.emp_no
-         JOIN salaries s on e.emp_no = s.emp_no AND s.to_date = last_salary.salary_max_to_date;
+               GROUP BY emp_no) last_sallary ON last_sallary.emp_no = e.emp_no
+         JOIN salaries s ON s.emp_no = e.emp_no AND s.to_date = last_sallary.sallary_max_to_date
+
+LIMIT 20;
 
 
 SELECT COUNT(*)
