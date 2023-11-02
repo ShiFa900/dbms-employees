c476a9f6a2a74112e82bb6f8dd876130e8ef1d32
Shifa <luh.shifa@gmail.com>
2023-11-02 14:15:30 +0800

    Thursday, Nov 02 2023

--- employees-query-09.sql
+++ employees-query-09.sql
@@ -1,42 +1,3 @@
-select e.emp_no                                 as ID,
-       concat(e.first_name, ' ', e.last_name)   as Name,
-       t.title                                  as Position,
-       t.from_date                              as "From",
-       t.to_date                                as "To",
-       if(e.gender = 'm', 'Male', 'Female')     as Gender,
-       e.hire_date                              as Hired,
-       d.dept_name                              as Department,
-       d.dept_no                                as "Department no",
-       de.from_date                             as "Assigned from",
-       de.to_date                               as "Assigned to",
-
-       concat(em.first_name, ' ', em.last_name) as Manager,
-       s.salary                                 as Salary
-
-from employees e
-
-         join(select tit.emp_no, MAX(tit.to_date) as max_title_to_date
-              from titles tit
-              group by tit.emp_no) as tit on tit.emp_no = e.emp_no
-
-
-         join titles t on e.emp_no = t.emp_no
-         join dept_emp de on de.emp_no = e.emp_no
-         join departments d on d.dept_no = de.dept_no
-         join dept_manager dm on d.dept_no = dm.dept_no
-         join employees em on em.emp_no = dm.emp_no
-         join salaries s on e.emp_no = s.emp_no
-
-
-where (
-                  t.to_date >= curdate() AND
-                  de.to_date >= curdate() AND
-                  dm.to_date >= curdate() AND
-                  s.to_date >= curdate())
-
-order by ID;
-
-
 SELECT e.emp_no                               AS ID,
        concat(e.first_name, ' ', e.last_name) AS Name,
        t.title                                AS Position,
@@ -49,7 +10,7 @@ SELECT e.emp_no                               AS ID,
        de.from_date                           AS "Assigned from",
        de.to_date                             AS "Assigned to",
 #
-       concat(em.first_name, ' ', em.last_name) as Manager,
+       concat(em.first_name, ' ', em.last_name) AS Manager,
        s.salary                               AS Salary
 FROM employees e
 
@@ -59,7 +20,7 @@ FROM employees e
                GROUP BY emp_no) last_title ON last_title.emp_no = e.emp_no
          JOIN titles t ON t.emp_no = e.emp_no AND t.to_date = last_title.titles_max_to_date
 
-         JOIN dept_emp de ON e.emp_no = de.emp_no
+         JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = last_title.titles_max_to_date
 
          JOIN departments d ON de.dept_no = d.dept_no
     #     Bagaimana menentukan employee A  dengan manager B?
@@ -77,9 +38,7 @@ FROM employees e
                GROUP BY emp_no) last_sallary ON last_sallary.emp_no = e.emp_no
          JOIN salaries s ON s.emp_no = e.emp_no AND s.to_date = last_sallary.sallary_max_to_date
 
-LIMIT 20;
+LIMIT 10;
 
 
-SELECT COUNT(*)
-FROM employees;
 
